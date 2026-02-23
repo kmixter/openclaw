392b63b5900b80a236f1ea00b77e174a19d477cb
Ken Mixter <kmixter@gmail.com>
2026-02-22 16:29:23 -0800

    feat: add configurable idle trigger system

--- src/gateway/server.impl.ts
+++ src/gateway/server.impl.ts
@@ -33,6 +33,7 @@ import { logAcceptedEnvOption } from "../infra/env.js";
 import { createExecApprovalForwarder } from "../infra/exec-approval-forwarder.js";
 import { onHeartbeatEvent } from "../infra/heartbeat-events.js";
 import { startHeartbeatRunner, type HeartbeatRunner } from "../infra/heartbeat-runner.js";
+import { startIdleTriggerRunner } from "../infra/idle-trigger-runner.js";
 import { getMachineDisplayName } from "../infra/machine-name.js";
 import { ensureOpenClawCliOnPath } from "../infra/path-env.js";
 import { setGatewaySigusr1RestartPolicy, setPreRestartDeferralCheck } from "../infra/restart.js";
@@ -755,6 +756,8 @@ export async function startGatewayServer(
       }
     : startHeartbeatRunner({ cfg: cfgAtStart });
 
+  let idleTriggerRunner = startIdleTriggerRunner({ cfg: cfgAtStart });
+
   const healthCheckMinutes = cfgAtStart.gateway?.channelHealthCheckMinutes;
   const healthCheckDisabled = healthCheckMinutes === 0;
   let channelHealthMonitor = healthCheckDisabled
@@ -959,6 +962,7 @@ export async function startGatewayServer(
             hooksConfig,
             hookClientIpConfig,
             heartbeatRunner,
+            idleTriggerRunner,
             cronState,
             browserControl,
             channelHealthMonitor,
@@ -967,6 +971,7 @@ export async function startGatewayServer(
             hooksConfig = nextState.hooksConfig;
             hookClientIpConfig = nextState.hookClientIpConfig;
             heartbeatRunner = nextState.heartbeatRunner;
+            idleTriggerRunner = nextState.idleTriggerRunner;
             cronState = nextState.cronState;
             cron = cronState.cron;
             cronStorePath = cronState.storePath;
@@ -1027,6 +1032,7 @@ export async function startGatewayServer(
     cron,
     heartbeatRunner,
     updateCheckStop: stopGatewayUpdateCheck,
+    idleTriggerRunner,
     nodePresenceTimers,
     broadcast,
     tickInterval,
